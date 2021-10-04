ffd0dccef43c8dab338c85f43af63f1976f90294
Vo Minh Don <vominhdona1@gmail.com>
2021-10-04 07:48:00 +0700

    Some update in test

--- ReviewExercise/Controllers/ItemDetailViewController.swift
+++ ReviewExercise/Controllers/ItemDetailViewController.swift
@@ -41,39 +41,47 @@ class ItemDetailViewController: UIViewController {
     }
     
     func roundComponent() {
-        itemBackgroundView.layer.cornerRadius = itemBackgroundView.bounds.size.height * 0.2
-        itemBackgroundView.layer.borderWidth = 2
-        itemBackgroundView.layer.borderColor = UIColor.white.cgColor
+//        itemBackgroundView.layer.cornerRadius = itemBackgroundView.bounds.size.height * 0.2
+//        itemBackgroundView.layer.borderWidth = 2
+//        itemBackgroundView.layer.borderColor = UIColor.white.cgColor
+        
         itemTypeView.layer.cornerRadius = itemTypeView.bounds.size.height * 0.5
         itemTypeView.layer.borderWidth = 1
         itemTypeView.layer.borderColor = UIColor.white.cgColor
+        
         plusButton.layer.cornerRadius = plusButton.bounds.size.height * 0.2
         plusButton.layer.borderWidth = 2
         plusButton.layer.borderColor = UIColor.white.cgColor
+        
         minusButton.layer.cornerRadius = minusButton.bounds.size.height * 0.2
         minusButton.layer.borderWidth = 2
         minusButton.layer.borderColor = UIColor.white.cgColor
-        quanlityView.layer.cornerRadius = quanlityView.bounds.size.height * 0.5
+        
+        //quanlityView.layer.cornerRadius = quanlityView.bounds.size.height * 0.5
+        
         sojaButton.layer.cornerRadius = sojaButton.bounds.size.height * 0.5
         sojaButton.layer.borderWidth = 2
         sojaButton.layer.borderColor = UIColor.white.cgColor
+        
         senfButton.layer.cornerRadius = senfButton.bounds.size.height * 0.5
         senfButton.layer.borderWidth = 2
         senfButton.layer.borderColor = UIColor.white.cgColor
+        
         advocadoButton.layer.cornerRadius = advocadoButton.bounds.size.height * 0.5
         advocadoButton.layer.borderWidth = 2
         advocadoButton.layer.borderColor = UIColor.white.cgColor
+        
         payButton.layer.cornerRadius = payButton.bounds.size.height * 0.5
         payButton.layer.borderWidth = 2
         payButton.layer.borderColor = UIColor.white.cgColor
     }
     
     func shadowComponent() {
-        itemBackgroundView.layer.shadowColor = UIColor.gray.cgColor
-        itemBackgroundView.layer.shadowOffset = CGSize(width: 7.0, height: 7.0)
-        itemBackgroundView.layer.shadowOpacity = 0.3
-        itemBackgroundView.layer.shadowRadius = 10.0
-        itemBackgroundView.layer.masksToBounds = false
+//        itemBackgroundView.layer.shadowColor = UIColor.gray.cgColor
+//        itemBackgroundView.layer.shadowOffset = CGSize(width: 7.0, height: 7.0)
+//        itemBackgroundView.layer.shadowOpacity = 0.3
+//        itemBackgroundView.layer.shadowRadius = 10.0
+//        itemBackgroundView.layer.masksToBounds = false
         
         itemTypeView.layer.shadowColor = UIColor.gray.cgColor
         itemTypeView.layer.shadowOffset = CGSize(width: 7.0, height: 7.0)
@@ -81,11 +89,11 @@ class ItemDetailViewController: UIViewController {
         itemTypeView.layer.shadowRadius = 5.0
         itemTypeView.layer.masksToBounds = false
         
-        quanlityView.layer.shadowColor = UIColor.gray.cgColor
-        quanlityView.layer.shadowOffset = CGSize(width: 7.0, height: 7.0)
-        quanlityView.layer.shadowOpacity = 0.3
-        quanlityView.layer.shadowRadius = 8.0
-        quanlityView.layer.masksToBounds = false
+//        quanlityView.layer.shadowColor = UIColor.gray.cgColor
+//        quanlityView.layer.shadowOffset = CGSize(width: 7.0, height: 7.0)
+//        quanlityView.layer.shadowOpacity = 0.3
+//        quanlityView.layer.shadowRadius = 8.0
+//        quanlityView.layer.masksToBounds = false
         
         sojaButton.layer.shadowColor = UIColor.gray.cgColor
         sojaButton.layer.shadowOffset = CGSize(width: 7.0, height: 7.0)
